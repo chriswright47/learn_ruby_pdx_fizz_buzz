0fa0a02220c9a38f1be81cae05a4d35aee1f6110
cwright <cwright@renewfund.com>
2015-04-02 15:43:19 -0700

    brute force solution

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,3 +1,11 @@
 class FizzBuzz
 
+  def fizz_buzzify(number)
+    result = String.new
+
+    result += 'fizz' if number % 3 == 0
+    result += 'buzz' if number % 5 == 0
+
+    result.empty? ? number : result
+  end
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,4 +1,31 @@
 require_relative "../spec_helper.rb"
 
 describe FizzBuzz do
+  subject { FizzBuzz.new }
+
+  describe "#fizz_buzzify" do
+    it "returns the number if divisible by neither three nor five" do
+      expect(subject.fizz_buzzify(1)).to eq 1
+    end
+
+    it "returns fizz if divisible by only three" do
+      expect(subject.fizz_buzzify(3)).to eq "fizz"
+    end
+
+    it "returns fizz if divisible by only three" do
+      expect(subject.fizz_buzzify(6)).to eq "fizz"
+    end
+
+    it "returns buzz if divisible by only five" do
+      expect(subject.fizz_buzzify(5)).to eq "buzz"
+    end
+
+    it "returns buzz if divisible by only five" do
+      expect(subject.fizz_buzzify(10)).to eq "buzz"
+    end
+
+    it "return fizzbuzz if divisible by both three and five" do
+      expect(subject.fizz_buzzify(15)).to eq "fizzbuzz"
+    end
+  end
 end
