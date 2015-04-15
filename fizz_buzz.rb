4dee5c4f5a41a5f6e347071a8c74c0e0510a3efd
cwright <cwright@renewfund.com>
2015-04-15 15:52:31 -0700

    work from in class on 2015-04-02

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,3 +1,14 @@
 class FizzBuzz
-
+  def fizz_buzzify(number)
+    case
+    when number % 15 == 0
+      "fizzbuzz"
+    when number % 3 == 0
+      "fizz"
+    when number % 5 == 0
+      "buzz"
+    else
+      number
+    end
+  end
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,4 +1,54 @@
 require_relative "../spec_helper.rb"
 
 describe FizzBuzz do
+  subject { FizzBuzz.new }
+
+  describe "#fizz_buzzify" do
+    it "returns fizz if equal to three" do
+      expect(subject.fizz_buzzify(3)).to eq "fizz"
+    end
+
+    it "returns fizz if divisible by three" do
+      expect(subject.fizz_buzzify(9)).to eq "fizz"
+    end
+
+    it "returns buzz if equal to five" do
+      expect(subject.fizz_buzzify(5)).to eq "buzz"
+    end
+
+    it "returns buzz if divisible by five" do
+      expect(subject.fizz_buzzify(10)).to eq "buzz"
+    end
+
+    it "returns fizzbuzz if divisible by both three and five" do
+      expect(subject.fizz_buzzify(15)).to eq "fizzbuzz"
+    end
+
+    it "returns fizzbuzz if divisible by both three and five" do
+      expect(subject.fizz_buzzify(30)).to eq "fizzbuzz"
+    end
+
+    it "return number if divisible by neither 3 nor 5" do
+      expect(subject.fizz_buzzify(7)).to eq 7
+    end
+  end
+
+  context "when divisors are passed in" do
+    subject { FizzBuzz.new(divisor_one: 7, divisor_two: 10) }
+
+    it "returns fizz if divisible by 7" do
+      pending "not implemented in class"
+      expect(subject.fizz_buzzify(7)).to eq "fizz"
+    end
+
+    it "returns buzz if divisible by 10" do
+      pending "not implemented in class"
+      expect(subject.fizz_buzzify(10)).to eq "buzz"
+    end
+
+    it "returns fizzbuzz if divisible by 7 and 10" do
+      pending "not implemented in class"
+      expect(subject.fizz_buzzify(70)).to eq "fizzbuzz"
+    end
+  end
 end
